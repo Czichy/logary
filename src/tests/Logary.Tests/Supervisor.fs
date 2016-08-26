bb558a072e831e0460221c4a98f7ab6ef03be719
Henrik Feldt <henrik@haf.se>
2016-08-26 17:01:43 +0200

    [tests] initial tests

--- src/tests/Logary.Tests/Supervisor.fs
+++ src/tests/Logary.Tests/Supervisor.fs
@@ -2,4 +2,16 @@
 
 open Logary
 open Hopac
+open Fuchu
 
+[<Tests>]
+let tests =
+  testList "Supervisor" [
+    testCase "create" <| fun _ ->
+      Supervisor.create () |> ignore
+
+    testCase "start and stop"
+      let sup = Supervisor.create ()
+      sup |> Supervisor.start |> run
+      sup |> Supervisor.stop |> run
+  ]
